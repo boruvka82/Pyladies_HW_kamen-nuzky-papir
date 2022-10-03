775b0b8c8ecc2ba82c355a1264ee24400f196f91
boruvka82 <102541983+boruvka82@users.noreply.github.com>
2022-10-03 11:20:43 +0200

    Update tah.py
    Doplň funkci tah_pocitace tak, aby brala jako argument symbol, za který má počítač hrát – buď 'x', nebo 'o'.

--- tah.py
+++ tah.py
@@ -1,6 +1,21 @@
-#cislo_policka(0,19)
-#symbol "x" or "o"
+from random import randrange
+
+def tah_pc(herni_pole, symbol):
+    "Vrátí herní pole se zaznamenaným tahem počítače"
+    
+    while True: 
+        cislo_policka = randrange(0,20)
+        print("pc:",cislo_policka)
+        if herni_pole[cislo_policka] != "-":
+            print("jiz obsazene, PC pokracuje...")      
+        else:    
+            return tah(herni_pole, cislo_policka, symbol)
+
 def tah(herni_pole, cislo_policka, symbol):
     """Vrátí herní pole s daným symbolem umístěným na danou pozici"""
     return herni_pole[:cislo_policka] + symbol + herni_pole[cislo_policka + 1:]
-print(tah("-----------------", 19, "x"))
+
+
+symbol = "o"
+herni_pole = ("--------------------")
+tah_pc(herni_pole, symbol)
